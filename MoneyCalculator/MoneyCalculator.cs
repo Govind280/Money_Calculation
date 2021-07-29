efefab401839ad899ca5739efb7ea2fc13d02099
Govind280 <govind.bgr@outlook.com>
2021-07-29 09:44:19 +0100

    Added exception handling

--- MoneyCalculator/MoneyCalculator.cs
+++ MoneyCalculator/MoneyCalculator.cs
@@ -10,38 +10,52 @@ namespace MoneyCalculator
     {
         public Money Max(IEnumerable<Money> monies)
         {
-            if (monies == null || !monies.Any())
-                return null;
+            try
+            {
+                if (monies == null || !monies.Any())
+                    return null;
 
-            int currencyCount =  monies.Select(x => x.Currency).Distinct().Count();
+                int currencyCount = monies.Select(x => x.Currency).Distinct().Count();
 
-            if (currencyCount > 1)
-                throw new ArgumentException("All monies are not in the same currency.");
+                if (currencyCount > 1)
+                    throw new ArgumentException("All monies are not in the same currency.");
 
-            var result = monies.OrderByDescending(a => a.Amount).FirstOrDefault();
+                var result = monies.OrderByDescending(a => a.Amount).FirstOrDefault();
 
-            return result;
+                return result;
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
         }
 
         public IEnumerable<Money> SumPerCurrency(IEnumerable<Money> monies)
         {
-            if (monies == null || !monies.Any())
-                return null;
-
-            IEnumerable<string> currencyCodes = monies.Select(x => x.Currency).Distinct();
-
-            List<Money> sumOfMonies = new List<Money>();
-
-            foreach(string currency in currencyCodes)
+            try
             {
-                sumOfMonies.Add(new()
-                {
-                    Currency = currency,
-                    Amount = monies.Where(a => a.Currency == currency).Select(x => x.Amount).Sum()
-                });
-            }
+                if (monies == null || !monies.Any())
+                    return null;
 
-            return sumOfMonies;
+                IEnumerable<string> currencyCodes = monies.Select(x => x.Currency).Distinct();
+
+                List<Money> sumOfMonies = new List<Money>();
+
+                foreach (string currency in currencyCodes)
+                {
+                    sumOfMonies.Add(new()
+                    {
+                        Currency = currency,
+                        Amount = monies.Where(a => a.Currency == currency).Select(x => x.Amount).Sum()
+                    });
+                }
+
+                return sumOfMonies;
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
